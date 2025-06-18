09c3ef65cc86297e1fa3edc81f5b9c50f9314b35
vanshgitraj <vanshraj01089@gmail.com>
2025-06-18 16:50:19 +0530

    Create components/BootScreen.tsx

---
+++ onedesk-os-layout/code/src/components/BootScreen.tsx
@@ -0,0 +1,10 @@
+// src/components/BootScreen.tsx 
+import React from 'react';
+
+const BootScreen: React.FC = () => (
+
+  <div className="flex items-center justify-center h-screen bg-black text-white text-2xl">
+    Booting OneDesk OS...
+  </div>
+);export default BootScreen;
+
