ee66f7f0e7e4326c56392b722220df37fbd762ee
vanshgitraj <vanshraj01089@gmail.com>
2025-06-18 16:33:40 +0530

    Update App.tsx

--- onedesk-os-layout/code/src/App.tsx
+++ onedesk-os-layout/code/src/App.tsx
@@ -1,4 +1,5 @@
-// src/App.tsx import React from 'react'; import { BrowserRouter, Routes, Route } from 'react-router-dom'; import Home from './pages/Home'; import OneFinance from './pages/OneFinance'; import Settings from './pages/Settings'; import TopBar from './components/TopBar'; import ThemeToggle from './components/ThemeToggle';
+// src/App.tsx 
+import React from 'react'; import { BrowserRouter, Routes, Route } from 'react-router-dom'; import Home from './pages/Home'; import OneFinance from './pages/OneFinance'; import Settings from './pages/Settings'; import TopBar from './components/TopBar'; import ThemeToggle from './components/ThemeToggle';
 
 const App: React.FC = () => { return ( <BrowserRouter> <div className="min-h-screen"> <TopBar /> <div className="p-4"> <Routes> <Route path="/" element={<Home />} /> <Route path="/onefinance" element={<OneFinance />} /> <Route path="/settings" element={<Settings />} /> </Routes> </div> <ThemeToggle /> </div> </BrowserRouter> ); };
 
