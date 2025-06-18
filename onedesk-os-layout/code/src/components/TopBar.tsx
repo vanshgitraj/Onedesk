3424d167ab6779e5fbfe9997a59b98e2e0ee4b7a
vanshgitraj <vanshraj01089@gmail.com>
2025-06-18 16:52:08 +0530

    Create components/TopBar.tsx

---
+++ onedesk-os-layout/code/src/components/TopBar.tsx
@@ -0,0 +1,9 @@
+// src/components/TopBar.tsx 
+import React from 'react';
+
+const TopBar: React.FC = () => (
+
+  <header className="bg-blue-600 text-white px-6 py-3 shadow-md">
+    <h1 className="text-xl font-semibold">OneDesk</h1>
+  </header>
+);export default TopBar;
