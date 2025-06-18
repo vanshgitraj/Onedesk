3a10501121379304f83a892476a6b12d8f3c859e
vanshgitraj <vanshraj01089@gmail.com>
2025-06-18 18:15:49 +0530

    Create sampleData.ts

---
+++ onedesk-os-layout/code/src/utils/sampleData.ts
@@ -0,0 +1,16 @@
+{
+  "customerId": "CUST001",
+  "name": "Vansh Raj",
+  "cibilScore": 745,
+  "pan": "ABCDE1234F",
+  "dob": "2004-03-22",
+  "products": ["personalLoan", "creditCard"],
+  "suggestedOffers": [
+    {
+      "bank": "HDFC",
+      "product": "Overdraft",
+      "limit": 150000,
+      "interestRate": 13.75
+    }
+  ]
+}
