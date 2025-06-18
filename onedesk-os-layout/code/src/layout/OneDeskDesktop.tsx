cc4cf78788111168c59906133d587daf9a59c0c6
vanshgitraj <vanshraj01089@gmail.com>
2025-06-18 16:57:00 +0530

    Create layout/OneDeskDesktop.tsx

---
+++ onedesk-os-layout/code/src/layout/OneDeskDesktop.tsx
@@ -0,0 +1,9 @@
+// src/layout/OneDeskDesktop.tsx 
+import React from 'react';
+
+const OneDeskDesktop: React.FC = () => (
+
+  <div className="grid grid-cols-4 gap-4 p-6">
+    {/* App icons will be rendered here */}
+  </div>
+);export default OneDeskDesktop;
