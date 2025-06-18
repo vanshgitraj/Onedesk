640dc348aa96c06488fdac2382f5d7bcec17c636
vanshgitraj <vanshraj01089@gmail.com>
2025-06-18 16:46:59 +0530

    Create components/AppIcon.tsx

---
+++ onedesk-os-layout/code/src/components/AppIcon.tsx
@@ -0,0 +1,12 @@
+// src/components/AppIcon.tsx 
+import React from 'react';
+
+interface AppIconProps { name: string; icon: React.ReactNode; onClick?: () => void; }
+
+const AppIcon: React.FC<AppIconProps> = ({ name, icon, onClick }) => (
+
+  <div className="flex flex-col items-center cursor-pointer" onClick={onClick}>
+    <div className="p-4 rounded-2xl shadow-md bg-white dark:bg-gray-800">{icon}</div>
+    <span className="text-sm mt-2 dark:text-white">{name}</span>
+  </div>
+);export default AppIcon;
