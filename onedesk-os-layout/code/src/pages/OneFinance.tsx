a9a57289be61474faa3850fd04d44bf2e77f985c
vanshgitraj <vanshraj01089@gmail.com>
2025-06-18 18:25:18 +0530

    Update OneFinance.tsx

--- onedesk-os-layout/code/src/pages/OneFinance.tsx
+++ onedesk-os-layout/code/src/pages/OneFinance.tsx
@@ -1,11 +1,31 @@
-// src/pages/OneFinance.tsx 
-import React from 'react';
+// src/pages/OneFinance.tsx
 
-const OneFinance: React.FC = () => (
+import React from "react";
+import { AppIcon } from "../components/AppIcon";
+import { ThemeToggle } from "../components/ThemeToggle";
+import { TopBar } from "../components/TopBar";
 
-  <div>
-    <h2 className="text-xl font-bold">OneFinance</h2>
-    <p className="mt-2 text-gray-600">All loan and finance services at one place.</p>
-  </div>
-);export default OneFinance;
+const OneFinance: React.FC = () => {
+  return (
+    <div className="h-screen w-screen flex flex-col bg-background text-foreground">
+      {/* 🔝 Top Navigation */}
+      <TopBar title="One Finance" />
 
+      {/* 💳 Main Finance Features */}
+      <div className="flex-1 flex flex-col items-center justify-center p-6 space-y-6">
+        <AppIcon appName="Loan Advisor" />
+        <AppIcon appName="CIBIL Analyzer" />
+        <AppIcon appName="Balance Transfer" />
+        <AppIcon appName="Overdraft Optimizer" />
+        {/* Add more finance tools here */}
+      </div>
+
+      {/* 🌗 Theme Switcher */}
+      <div className="absolute bottom-4 right-4">
+        <ThemeToggle />
+      </div>
+    </div>
+  );
+};
+
+export default OneFinance;
