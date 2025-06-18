d1e23afa8a5d212fe95c536b8af161e8336fc05e
vanshgitraj <vanshraj01089@gmail.com>
2025-06-18 16:58:32 +0530

    Create pages/Home.tsx

---
+++ onedesk-os-layout/code/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+// src/pages/Home.tsx import React from 'react';
+
+const Home: React.FC = () => (
+
+  <div>
+    <h2 className="text-xl font-bold">Welcome to OneDesk</h2>
+    <p className="mt-2 text-gray-600">Click an app icon to open a product.</p>
+  </div>
+);export default Home;
+
+
