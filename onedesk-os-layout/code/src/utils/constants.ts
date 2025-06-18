92943e60e27c4f383d44367d6c46451a6dd862f2
vanshgitraj <vanshraj01089@gmail.com>
2025-06-18 17:04:08 +0530

    Update constants.ts

--- onedesk-os-layout/code/src/utils/constants.ts
+++ onedesk-os-layout/code/src/utils/constants.ts
@@ -1,1 +1,2 @@
-// src/utils/constants.ts export const APP_NAME = 'OneDesk'; export const VERSION = '1.0.0';
+// src/utils/constants.ts 
+export const APP_NAME = 'OneDesk'; export const VERSION = '1.0.0';
